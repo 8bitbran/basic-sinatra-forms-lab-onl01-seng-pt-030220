4efee7ff70cf6e94bbbbb95e08584c16e91e13a1
8bitbran <brandonmar97@gmail.com>
2020-05-28 20:44:11 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require 'sinatra/base'
 
-class App < Sinatra::Base
+class App < Sinatra::Base 
 
+    get '/newteam' do 
+        erb :newteam
+    end 
+
+    post '/team' do 
+        @all_params = params 
+
+        erb :team
+    end 
 
 end
